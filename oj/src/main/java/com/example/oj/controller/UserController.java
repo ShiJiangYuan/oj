16f59b83ed5f3dad18411adf79a2eaec5c357860
ShiJiangYuan <2577380101@qq.com>
2022-03-22 14:35:30 +0800

    update avatar path

--- oj/src/main/java/com/example/oj/controller/UserController.java
+++ oj/src/main/java/com/example/oj/controller/UserController.java
@@ -66,7 +66,8 @@ public class UserController {
     private String serverPort;
 
     // 文件存储路径
-    private String fileUploadPath = (new File("src/main/resources/static/img")).getAbsolutePath() + "\\";
+    @Value("${oj.avatar}")
+    private String fileUploadPath;
 
 
     /**
